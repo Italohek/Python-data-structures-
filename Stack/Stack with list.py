f41aac95f74efc6168bc8d0fb9adc0a9d0bda914
Italohek <132797328+Italohek@users.noreply.github.com>
2023-11-16 12:29:19 -0300

    Create Stack with list.py

---
+++ Stack/Stack with list.py
@@ -0,0 +1,25 @@
+class Stack:
+    def _init_(self):
+        self.items = []
+
+    #Return True if the Stack is empty
+    def empty(self):
+        return len(self.items) == 0
+    
+    #Adds a value to the Stack
+    def add(self, item):
+        self.items.append(item)
+
+    #Withdraw the last value from the stack
+    def poptop(self):
+        if not self.empty():
+            return self.items.pop()
+
+    #Check what value is at the top of the Stack
+    def top(self):
+        if not self.empty():
+            return self.items[-1]
+
+    #Return the len of the stack
+    def size(self):
+        return len(self.items)
